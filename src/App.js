9cb5d6895adb8f176fbbc516fd71e970c91c5539
Prem Prakash Shinde <shindeprem102@gmail.com>
2023-08-24 20:06:51 +0530

    React Query

--- src/App.js
+++ src/App.js
@@ -6,33 +6,37 @@ import Menu from "./components/Menu";
 import Contact from "./components/Contact";
 import { Link } from "react-router-dom";
 import { createContext, useState } from "react";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
 
 export const AppContext = createContext();
 
 function App() {
   const [userName, setUserName] = useState("Prem");
+  const client = new QueryClient();
   return (
     <div className='App'>
-      <AppContext.Provider value={{ userName, setUserName }}>
-        <Router>
-          <div>
-            <Link to='/'>Home</Link>
-            <Link to='/profile'>Profile</Link>
-            <Link to='/contact'>Contact</Link>
-          </div>
-          <Routes>
-            {/* <Route path='/' element={<Home userName={userName} />} /> */}
-            <Route path='/' element={<Home />} />
-            <Route
-              path='/profile'
-              // element={<Menu userName={userName} setUserName={setUserName} />}
-              element={<Menu userName={userName} />}
-            />
-            <Route path='/contact' element={<Contact />} />
-            <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
-          </Routes>
-        </Router>
-      </AppContext.Provider>
+      <QueryClientProvider client={client}>
+        <AppContext.Provider value={{ userName, setUserName }}>
+          <Router>
+            <div>
+              <Link to='/'>Home</Link>
+              <Link to='/profile'>Profile</Link>
+              <Link to='/contact'>Contact</Link>
+            </div>
+            <Routes>
+              {/* <Route path='/' element={<Home userName={userName} />} /> */}
+              <Route path='/' element={<Home />} />
+              <Route
+                path='/profile'
+                // element={<Menu userName={userName} setUserName={setUserName} />}
+                element={<Menu userName={userName} />}
+              />
+              <Route path='/contact' element={<Contact />} />
+              <Route path='*' element={<h1>PAGE NOT FOUND</h1>} />
+            </Routes>
+          </Router>
+        </AppContext.Provider>
+      </QueryClientProvider>
     </div>
   );
 }
--- src/components/Home.js
+++ src/components/Home.js
@@ -1,12 +1,32 @@
 import React, { useContext } from "react";
 import { AppContext } from "../App";
+import { useQuery } from "@tanstack/react-query";
+import Axios from "axios";
 
 // const Home = (props) => {
 //   return <div>username : {props.userName}</div>;
 // };
 const Home = () => {
+  const { data, isLoading, refetch } = useQuery(["cat"], () => {
+    return Axios.get("https://catfact.ninja/fact").then((res) => res.data);
+  });
   const { userName } = useContext(AppContext);
-  return <div>username : {userName}</div>;
+  if (isLoading) {
+    return <h1>Loding...</h1>;
+  }
+  return (
+    <>
+      <div>username : {userName}</div>
+      <p>{data?.fact}</p>
+      <button
+        onClick={() => {
+          refetch();
+        }}
+      >
+        Update
+      </button>
+    </>
+  );
 };
 
 export default Home;
